dfa4b4be2ebec0b2888098ff88e0a8026b8e7a77
Alan <Alan@ALANPC>
2017-05-12 21:54:29 +0200

    Project rename

--- NonogramSolver/Properties/AssemblyInfo.cs
+++ NonogramSolver/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EverTopZadanieV2_XML_Test")]
+[assembly: AssemblyTitle("NonogramSolver")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EverTopZadanieV2_XML_Test")]
+[assembly: AssemblyProduct("NonogramSolver")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
